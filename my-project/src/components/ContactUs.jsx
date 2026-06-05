6dc4ead9b817180a89a1b3107059aff679d89588
huzaifa tahir <h905923@gmail.com>
2026-06-05 22:58:41 +0500

    layouts fixes

--- my-project/src/components/ContactUs.jsx
+++ my-project/src/components/ContactUs.jsx
@@ -62,7 +62,7 @@ const ContactUs = () => {
         dark:text-white
       "
     >
-      <div className="flex  flex-col lg:flex-row items-center mx-auto px-6 lg:px-10">
+      <div className="flex  flex-col lg:flex-row flex-wrap justify-center items-center mx-auto px-6 lg:px-10">
         {/* Header */}
         <motion.div
           initial={{ opacity: 0, y: 25 }}
